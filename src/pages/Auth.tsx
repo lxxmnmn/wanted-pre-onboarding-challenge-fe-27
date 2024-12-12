6ace2aea6a6ae3ba1437d5369696c5f58fcbb03b
MINJEONG <lxxmnmn@gmail.com>
2024-12-12 14:28:03 +0900

    fix: 회원가입 폼 수정

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,105 +1,59 @@
-import { useState, ChangeEvent, useEffect } from 'react';
-import {
-  SelectChangeEvent,
-  FormControl,
-  TextField,
-  MenuItem,
-  Button,
-  Select,
-} from '@mui/material';
-import { UserType } from '../types';
-
-const DOMAIN_LIST: string[] = [
-  'gmail.com',
-  'naver.com',
-  'daum.net',
-  'hanmail.net',
-  'icloud.com',
-  'nate.com',
-];
+import { useState, useEffect, ChangeEvent } from 'react';
+import { User } from '../types';
+import { useDebounce } from '../hooks';
 
 const Auth = () => {
-  const [user, setUser] = useState<UserType>({ email: '', password: '' });
+  const [user, setUser] = useState<User>({ email: '', password: '' });
   const [isFilled, setIsFilled] = useState<boolean>(false);
 
-  const regex: RegExp = /^[a-zA-Z0-9]+$/;
+  const debouncedEmail = useDebounce(user.email, 200);
+  const debouncedPassword = useDebounce(user.password, 200);
 
-  const setUserInfo = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-
-    if (regex.test(value) && (name !== 'password' || value.length >= 8)) {
-      setUser((prevUser) => ({
-        ...prevUser,
-        [name]: value,
-      }));
-    } else {
-      alert(`${name}을 다시 입력해주세요.`);
-    }
-  };
-
-  const generateEmail = (event: SelectChangeEvent<string>) => {
-    const id = user.email;
-    const fullEmail = id.concat('@', event.target.value);
-
+  const changeEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setUser((prevUser) => ({
       ...prevUser,
-      email: fullEmail,
+      email: event.target.value,
+    }));
+  };
+
+  const changePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setUser((prevUser) => ({
+      ...prevUser,
+      password: event.target.value,
     }));
   };
 
   useEffect(() => {
-    if (user.email.includes('@') && user.password.length > 0) {
-      setIsFilled(true);
-    } else {
-      setIsFilled(false);
-    }
-  }, [user]);
+    const emailRegex: RegExp =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid =
+      emailRegex.test(debouncedEmail) && debouncedPassword.length >= 8;
+
+    setIsFilled(isValid);
+  }, [debouncedEmail, debouncedPassword]);
 
   return (
-    <FormControl>
-      <div>
-        <TextField
+    <div>
+      <form>
+        <input
           type="text"
-          name="email"
-          sx={{ m: 1 }}
-          label="Email"
-          helperText="영문, 숫자만 입력해주세요."
-          variant="standard"
-          onChange={setUserInfo}
+          placeholder="이메일 형태로 입력해주세요."
+          onChange={changeEmail}
         />
-        <span>@</span>
-        <Select
-          sx={{ m: 1, width: '150px' }}
-          defaultValue=""
-          onChange={generateEmail}
-        >
-          {DOMAIN_LIST.map((value, index) => (
-            <MenuItem key={index} value={value}>
-              {value}
-            </MenuItem>
-          ))}
-        </Select>
-      </div>
-      <div>
-        <TextField
+        <input
           type="password"
-          name="password"
-          sx={{ m: 1 }}
-          label="Password"
-          helperText="영문, 숫자를 8자 이상 입력해주세요."
-          variant="standard"
-          onChange={setUserInfo}
+          placeholder="8자 이상 입력해주세요."
+          onChange={changePassword}
         />
-      </div>
-      <Button
-        sx={{ m: 2 }}
-        variant="contained"
-        disabled={!isFilled}
-        onClick={() => console.log(user)}
-      >
-        회원 가입
-      </Button>
-    </FormControl>
+        <button
+          type="button"
+          disabled={!isFilled}
+          onClick={() => console.log(user)}
+        >
+          회원 가입
+        </button>{' '}
+      </form>
+    </div>
   );
 };
 
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,4 @@
-export interface UserType {
+export interface User {
   email: string;
   password: string;
 }
