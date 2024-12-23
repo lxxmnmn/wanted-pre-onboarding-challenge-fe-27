53d3d77c48ea3d43f670755d6008d66139c98778
MINJEONG <lxxmnmn@gmail.com>
2024-12-23 10:10:10 +0900

    fix: 페이지 라우팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Toast } from '~components/Toast';
 import { Account } from '~pages/Account';
@@ -11,7 +11,8 @@ function App() {
     <Router>
       <Toast />
       <Routes>
-        <Route path="/" element={<TodoList />} />
+        <Route path="/" element={<Navigate to={'/auth'} replace />} />
+        <Route path="/todos" element={<TodoList />} />
         <Route path="/auth" element={<Account />} />
       </Routes>
     </Router>
